e2b2177f6d0f44f0cd5de427ca6f19aa775cef37
kaylals <shuoliu10@gmail.com>
2024-10-15 22:51:57 -0700

    Initial commit

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oagdy1z7ciOn25OH5d7',
+        issuer: 'https://dev-42102830.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
